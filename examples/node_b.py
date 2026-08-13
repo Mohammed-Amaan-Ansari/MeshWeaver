46a6452355104c6175901445d2e3e724959c8653
Mohammed-Amaan-Ansari <ansari.mdamaan1@gmail.com>
2026-08-13 22:55:44 +0530

    Updated node_b.py

--- examples/node_b.py
+++ examples/node_b.py
@@ -4,6 +4,12 @@ from meshweaver.node import MeshNode
 
 async def main():
     node = MeshNode("127.0.0.1", 9002)
+
+    # Send a ping to Node A
+    asyncio.create_task(
+        node.send_ping("127.0.0.1", 9001)
+    )
+
     await node.start()
 
 
