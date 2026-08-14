d2bbef072a3b4b43f1501a7b7a0dcc7df7f627b1
Mohammed-Amaan-Ansari <ansari.mdamaan1@gmail.com>
2026-08-14 23:18:00 +0530

    Updated Discovery.py

--- meshweaver/network/discovery.py
+++ meshweaver/network/discovery.py
@@ -15,3 +15,11 @@ async def send_hello(node, peer):
     )
 
     print(f"👋 HELLO sent to {peer}")
+
+async def discovery_loop(node, bootstrap_peers):
+    while True:
+        for peer in bootstrap_peers:
+            if peer != (node.host, node.port):
+                await send_hello(node, peer)
+
+        await asyncio.sleep(10)
