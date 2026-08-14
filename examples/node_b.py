9afed36f99d2548235eb2e1f03c19695f2ee9164
Mohammed-Amaan-Ansari <ansari.mdamaan1@gmail.com>
2026-08-14 23:20:56 +0530

    Day 04: Add automatic peer discovery with HELLO/WELCOME protocol

--- examples/node_b.py
+++ examples/node_b.py
@@ -3,11 +3,12 @@ from meshweaver.node import MeshNode
 
 
 async def main():
-    node = MeshNode("127.0.0.1", 9002)
-
-    # Send a ping to Node A
-    asyncio.create_task(
-        node.send_ping("127.0.0.1", 9001)
+    node = MeshNode(
+        host="127.0.0.1",
+        port=9002,
+        bootstrap_peers=[
+            ("127.0.0.1", 9001),
+        ],
     )
 
     await node.start()
