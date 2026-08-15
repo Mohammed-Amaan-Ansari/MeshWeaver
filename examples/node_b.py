0abb6d0b3b280385b7d972be0b5be703faf396cc
Mohammed-Amaan-Ansari <ansari.mdamaan1@gmail.com>
2026-08-15 22:49:16 +0530

    Updated node_b.py

--- examples/node_b.py
+++ examples/node_b.py
@@ -1,17 +1,21 @@
 import asyncio
+
 from meshweaver.node import MeshNode
 
 
 async def main():
+
     node = MeshNode(
         host="127.0.0.1",
         port=9002,
+        node_id="NODE_B",
         bootstrap_peers=[
-            ("127.0.0.1", 9001),
+            ("127.0.0.1", 9001)
         ],
     )
 
     await node.start()
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
