d37b02a2beb5ac4a5f877bc6d10e12bdc6f51ef1
Mohammed-Amaan-Ansari <ansari.mdamaan1@gmail.com>
2026-08-22 23:37:18 +0530

    updated discovery.py

--- meshweaver/network/discovery.py
+++ meshweaver/network/discovery.py
@@ -4,9 +4,13 @@ import json
 HELLO = "HELLO"
 WELCOME = "WELCOME"
 TASK = "TASK"
+RESULT = "RESULT"
 
 
-def create_hello(node_id, port):
+def create_hello(
+    node_id,
+    port,
+):
 
     return {
         "type": HELLO,
@@ -15,7 +19,10 @@ def create_hello(node_id, port):
     }
 
 
-def create_welcome(node_id, port):
+def create_welcome(
+    node_id,
+    port,
+):
 
     return {
         "type": WELCOME,
@@ -24,14 +31,36 @@ def create_welcome(node_id, port):
     }
 
 
-def encode_message(message):
+def create_result(
+    sender_id,
+    task_id,
+    status,
+    result=None,
+    error=None,
+):
+
+    return {
+        "type": RESULT,
+        "sender_id": sender_id,
+        "task_id": task_id,
+        "status": status,
+        "result": result,
+        "error": error,
+    }
+
+
+def encode_message(
+    message,
+):
 
     return json.dumps(
         message
     ).encode("utf-8")
 
 
-def decode_message(data):
+def decode_message(
+    data,
+):
 
     return json.loads(
         data.decode("utf-8")
